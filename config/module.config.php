10a9e5d66129c076eb83793396403d6bd81bc3f9
Eli Alejandro <iscelialejandro@gmail.com>
2019-02-18 00:25:07 -0600

    New config for noty

--- config/module.config.php
+++ config/module.config.php
@@ -1,17 +1,15 @@
 <?php
 
 return [
-    'noty_assets' => [
-        // Noty library
-        'library'   => 'js/noty/jquery.noty.packaged.js',
-
-        // Custom configuration
-        'config'    => 'js/noty/jquery.noty.config.js',
-
-        // If you use Noty v3+
-        'useNotyV3' => false,
-        'theme'     => 'metroui',
-        'css'       => ''
+    
+    'noty_config' => [
+        'layout'    => 'topRight',
+        'theme'     => 'mint',
+        'closeWith' => ['click', 'button'],
+        /* 'animation' => [
+            'open'  => 'animated fadeInRight',
+            'close' => 'animated fadeOutRight'
+        ] */
     ],
 
     'view_helpers' => [
